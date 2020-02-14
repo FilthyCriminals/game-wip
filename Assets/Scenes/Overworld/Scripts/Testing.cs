54501b92cb4020d130796e4a2f3a0f70458329af
Chris <cobb.chris.l@gmail.com>
2020-02-13 21:51:40 -0500

    Added tile based grid map for overworld navigation

---
+++ Assets/Scenes/Overworld/Scripts/Testing.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour {
+
+    private Grid grid;
+
+    private void Start() {
+        grid = new Grid(10, 10, 10f, new Vector3(0, 0));
+    }
+
+    private void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            grid.SetValue(Utilities.GetMouseWorldPosition(), 1);
+        }
+
+        if (Input.GetMouseButtonDown(1)) {
+            grid.GetValue(Utilities.GetMouseWorldPosition());
+        }
+    }
+}
