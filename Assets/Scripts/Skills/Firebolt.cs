baa1743b55e6c164cf020896fdb936c18b5467af
AnthonyHuber <anthonyhuber57@gmail.com>
2020-03-19 18:21:57 -0400

    Added stunning fist skill

--- Assets/Scripts/Skills/Firebolt.cs
+++ Assets/Scripts/Skills/Firebolt.cs
@@ -1,21 +1,25 @@
 ﻿using System.Collections;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "NewSkill", menuName = "Skill")]
+[CreateAssetMenu(fileName = "Firebolt", menuName = "Skill/Firebolt")]
 public class Firebolt : Skill {
+
+	[SerializeField] protected float speed;
+
+	[SerializeField] protected GameObject VFXPrefab;
 
 	private System.Random rand = new System.Random();
 
 	public override IEnumerator Cast(BattleEntityController caster, BattleEntityController target) {
 
-		this.VFXPrefab.transform.position = caster.transform.position;
+		VFXPrefab.transform.position = caster.transform.position;
 
-		Transform source = Instantiate(this.VFXPrefab).transform;
+		Transform source = Instantiate(VFXPrefab).transform;
 
 		yield return new WaitForSeconds(0.75f);
 
 		while (Vector3.Distance(source.position, target.transform.position) > 0.001f) {
-			source.position = Vector3.MoveTowards(source.position, target.transform.position, this.speed * Time.deltaTime);
+			source.position = Vector3.MoveTowards(source.position, target.transform.position, speed * Time.deltaTime);
 			yield return null;
 		}
 
---
+++ Assets/Scripts/Skills/StunningFist.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "StunningFist", menuName = "Skill/StunningFist")]
+public class StunningFist : Skill {
+	[SerializeField] protected int duration;
+
+	private System.Random rand = new System.Random();
+
+	public override IEnumerator Cast(BattleEntityController caster, BattleEntityController target) {
+
+		yield return new WaitForSeconds(1.5f);
+
+		int damage = rand.Next(this.minPower, this.maxPower + 1);
+
+		target.TakeDamage(damage);
+		target.SetStatusEffect(StatusEffect.STUN, duration);
+	}
+}
